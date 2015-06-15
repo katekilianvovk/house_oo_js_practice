60ff744bffe543325b850aa51f61b4bfe45d6d23
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-06-15 11:57:02 -0600

    House specs passing

--- house.js
+++ house.js
@@ -1,6 +1,14 @@
-var House = function(){
+var House = function(numOfBedrooms){
+  this.numOfBedrooms = numOfBedrooms;
+  this.rooms = {};
+};
 
+House.prototype.getRooms = function() {
+  return this.rooms;
+};
 
+House.prototype.addRoom = function(room) {
+  this.rooms[room["name"]] = room.contents;
 };
 
 
