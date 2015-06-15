e0b60f62c21267216047101a4d7e90cd0ef5c686
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-06-15 11:22:25 -0600

    Room specs are passing

--- room.js
+++ room.js
@@ -1,7 +1,21 @@
-var Room = function(){
-
-
+var Room = function(name, width, length){
+  this.contents = [];
+  this.name = name;
+  this.width = width;
+  this.length = length;
 };
 
+Room.prototype.addFurniture = function(furniture) {
+  if (this.contents.indexOf(furniture) !== -1) {
+    return "sorry, you can't have two of those";
+  } else {
+    this.contents.push(furniture);
+  }
+};
+
+Room.prototype.removeFurniture = function() {
+  this.contents.pop();
+  return this.contents;
+};
 
 module.exports = Room;
