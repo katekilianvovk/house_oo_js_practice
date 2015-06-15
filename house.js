cd16619e4332ef46af79d0edce47b602038709be
katie Pekarek <katie_pekarek@yahoo.com>
2015-06-15 09:05:18 -0600

    cleared answers

--- house.js
+++ house.js
@@ -1,16 +1,7 @@
-var House = function(numOfBedrooms){
-  this.numOfBedrooms = numOfBedrooms;
-  this.rooms = {};
+var House = function(){
+
 
 };
 
-House.prototype.getRooms = function(){
-  return this.rooms;
-};
-
-House.prototype.addRoom = function(room){
-  return this.rooms[room.name] = [];
-
-};
 
 module.exports = House;
--- room.js
+++ room.js
@@ -1,25 +1,7 @@
-var Room = function(name, width, length){
-  this.name = name;
-  this.contents = [];
-  this.width = width;
-  this.length = length;
+var Room = function(){
+
 
 };
 
-Room.prototype.addFurniture = function(item){
-  if (this.contents.indexOf(item) == -1){
-    this.contents.push(item);
-    return this.contents;
-  } else {
-    return 'sorry, you can\'t have two of those'
-  }
-};
-
-Room.prototype.removeFurniture = function(item){
-  if (this.contents.indexOf(item) !== -1){
-    this.contents.splice(this.contents.indexOf(item), 1)
-  }
-  return this.contents;
-};
 
 module.exports = Room;
